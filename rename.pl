40ba54b5dedf0a8ad3db8498feea830ba593be87
yqlan <74458322+yqlan@users.noreply.github.com>
2021-09-06 11:08:15 +0800

    Update rename.pl

--- rename.pl
+++ rename.pl
@@ -5,13 +5,13 @@ use warnings;
 die "Usage:perl $0 <oldpattern> <newpattern> \n"if(@ARGV!=2);
 my ($oldpattern,$newpattern)=@ARGV;
 foreach my $file (glob "*"){
-my $newfile = $file;
-$newfile =~ s/$oldpattern/$newpattern/;
-if(-e $newfile){
-warn "Can't rename $file to $newfile. The $newfile exists!\n";
-}elsif(rename $file => $newfile){
-}else{
-warn "Rename $file to $newfile failed: $!\n"; 
-}
-}
+				my $newfile = $file;
+				$newfile =~ s/$oldpattern/$newpattern/;
+				if(-e $newfile){
+						warn "Can't rename $file to $newfile. The $newfile exists!\n";
+						}
+				elsif(rename $file => $newfile){}
+				else{
+					warn "Rename $file to $newfile failed: $!\n";}
+			}
 	                                  	       	              
